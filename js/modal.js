48c350a288cf96b0725fadaee5541104aaf20c28
Alexey Indyuchenko <46484506+AlexeyGasay@users.noreply.github.com>
2022-07-14 19:29:25 +0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,2 @@
+"use strict";var navContainer=document.getElementById("header_nav_container"),isVisible=!1,showMenu=function(){console.log(1),1==(isVisible=!isVisible)?(navContainer.style.visibility="visible",navContainer.style.opacity="1"):(navContainer.style.visibility="hidden",navContainer.style.opacity="0")};
+//# sourceMappingURL=modal.js.map
